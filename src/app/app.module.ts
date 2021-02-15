43ac0c9059d902cf139026b109536109b287406e
derdesz <dora.mailbox@gmail.com>
2021-02-15 15:55:26 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {HeaderComponent} from './header.component/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
     declarations: [
-        AppComponent,
-        HeaderComponent,
-        RecipesComponent,
-        RecipeListComponent,
-        RecipeDetailComponent,
-        RecipeItemComponent
+        AppComponent
     ],
   imports: [
     BrowserModule

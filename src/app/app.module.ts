e4831fc664650e5a45cff5b2d2acd77eeeace6f5
derdesz <dora.mailbox@gmail.com>
2021-02-15 17:16:24 +0100

    add imports to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {HeaderComponent} from './header.component/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
+import {RecipesComponent} from './recipes/recipes.component';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
+import {RecipeListComponent} from './recipes/recipe-list/recipe-list.component';
+import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
+import {RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        HeaderComponent,
-        RecipesComponent
-    ],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent
+  ],
   imports: [
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}

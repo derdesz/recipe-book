43ebfed65d3d6d7f9bfa74d5bcbd3e03516cbd24
derdesz <dora.mailbox@gmail.com>
2021-02-15 17:13:22 +0100

    add new recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {RecipeModel} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: RecipeModel[] = [
+    new RecipeModel('Test recipe', 'this is simply a test', 'https://media4.s-nbcnews.com/j/newscms/2020_16/1558143/martha-recipes-today-main-200413_41de0fe80755b18142fdcfefdeca654a.today-inline-large2x.jpg')
+  ];
 
   constructor() { }
 
